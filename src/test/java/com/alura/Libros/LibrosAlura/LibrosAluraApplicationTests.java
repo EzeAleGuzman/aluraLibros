7b52c985be6cda6e7f85f8627161025f777f3837
EzeAleGuzman <ezealeguzman@gmail.com>
2024-07-07 18:15:03 -0300

    Primer Commit

---
+++ src/test/java/com/alura/Libros/LibrosAlura/LibrosAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.Libros.LibrosAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
